543161bea8300a521694f6d4a6feb7c2eb42d008
Sebastian Barcia Beltran <sebastianbarciabeltran@gmail.com>
2022-11-28 16:05:32 +0100

    created the structure of the project and implement the lazyLoad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'products',
+    loadChildren: () => import('./products/products.module').then( m => m.ProductsModule )
+  },
+  {
+    path: '**',
+    redirectTo: 'products'
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
